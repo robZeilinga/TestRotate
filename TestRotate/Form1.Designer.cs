5ab5070aa76d66509c08ba0b62d23be423048c92
rob <robert.zeilinga@standardbank.co.za>
2018-05-16 12:14:13 +0200

    Inital Commit 2

---
+++ TestRotate/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace TestRotate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(14, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(332, 422);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Image File";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(72, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(286, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(364, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(28, 20);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "... ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(417, 134);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(48, 36);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(462, 102);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(37, 26);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "0.1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(497, 70);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(37, 26);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "1";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(381, 102);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(37, 26);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "0.1";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(352, 70);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(37, 26);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "1";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(414, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Rotation";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 562);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ TestRotate/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestRotate
+{
+    public partial class Form1 : Form
+    {
+
+        string fileName = "";
+        string  initialImg = "";
+        Image img = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            openFileDialog1.InitialDirectory = @"C:\Users\rober\Pictures\";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult dr =  openFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK || dr == DialogResult.Yes)
+            {
+                fileName = openFileDialog1.FileName;
+                pictureBox1.Load(fileName);
+                initialImg = fileName;
+                img = pictureBox1.Image;
+            }
+        }
+
+        // reset image
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Load(initialImg);
+            img = pictureBox1.Image;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            img = RotateImage(img, 1, 1);
+            pictureBox1.Image = img;
+            
+        }
+
+        // rotate image from: http://code-bude.net/2011/07/12/bilder-rotieren-mit-csharp-bitmap-rotateflip-vs-gdi-graphics/
+        public static Image RotateImage(Image img, float rotationAngle, float zoom)
+        {
+            using (Graphics gfx = Graphics.FromImage(img))
+            {
+                gfx.TranslateTransform((float)img.Width / 2, (float)img.Height / 2);
+                gfx.RotateTransform(rotationAngle);
+                gfx.ScaleTransform(zoom, zoom);
+                gfx.TranslateTransform(-(float)img.Width / 2, -(float)img.Height / 2);
+                gfx.DrawImage(img, new Point(0, 0));
+            }
+            return img;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            img = RotateImage(img, -1, 1);
+            pictureBox1.Image = img;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            img = RotateImage(img, 0.1f, 1);
+            pictureBox1.Image = img;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            img = RotateImage(img, -0.1f, 1);
+            pictureBox1.Image = img;
+        }
+    }
+}
